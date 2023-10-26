6266629aacc68e4bcc8f10ab34a25f748860e73f
Your Name <mademn@ufl.edu>
2023-10-26 18:34:42 -0400

    message

--- encoder.py
+++ encoder.py
@@ -1,24 +1,34 @@
+# Nihitha Madem
+def menu():
+    print('Menu')
+    print('-------------')
+    print('1. Encode \n2. Decode \n3. Quit')
 def encoder(password):
     password_new = ''
     for i in password:
         password_increment = int(i) + 3
         password_new += str(password_increment)
-    print(password_new)
     return password_new
+def decoder(password_new):
+    password_old = ''
+    for i in password_new:
+        password_increment = int(i) - 3
+        password_old += str(password_increment)
+    return password_old
 def main():
-    print('Menu')
-    print('-------------')
-    print('1. Encode \n2. Decode \n3. Quit')
-    menu_option = int(input('Please enter an option: '))
     game_continue = True
     while game_continue:
+        menu()
+        menu_option = int(input('Please enter an option: '))
         if menu_option == 1:
             choose_password = input('Please enter your password to encode: ')
             encoder(choose_password)
             print('Your password has been encoded and stored!')
         elif menu_option == 2:
-            pass
+            print(f'The encoded password is {encoder(choose_password)}, and the original password is {decoder(encoder(choose_password))}')
         else:
             break
 if __name__ == '__main__':
     main()
+
+
