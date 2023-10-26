fb9242daad39c963834436a1374402606f3b8b5f
Your Name <mademn@ufl.edu>
2023-10-26 18:37:02 -0400

    message

--- encoder.py
+++ encoder.py
@@ -9,12 +9,6 @@ def encoder(password):
         password_increment = int(i) + 3
         password_new += str(password_increment)
     return password_new
-def decoder(password_new):
-    password_old = ''
-    for i in password_new:
-        password_increment = int(i) - 3
-        password_old += str(password_increment)
-    return password_old
 def main():
     game_continue = True
     while game_continue:
@@ -25,10 +19,11 @@ def main():
             encoder(choose_password)
             print('Your password has been encoded and stored!')
         elif menu_option == 2:
-            print(f'The encoded password is {encoder(choose_password)}, and the original password is {decoder(encoder(choose_password))}')
+            pass
         else:
             break
 if __name__ == '__main__':
     main()
+
 
 
