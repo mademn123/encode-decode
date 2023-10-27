c9a136f3ce477bfedd4b434f682190b63e5e19d1
kayleem4rie <kpekar@ufl.edu>
2023-10-26 20:42:25 -0400

    kaylee

--- encoder.py
+++ encoder.py
@@ -9,6 +9,15 @@ def encoder(password):
         password_increment = int(i) + 3
         password_new += str(password_increment)
     return password_new
+
+# Kaylee Pekar added this function.
+def decode(password):
+    decoded_password = ''
+    for i in password:
+        password_decrement = int(i) - 3
+        decoded_password += str(password_decrement)
+    return decoded_password
+
 def main():
     game_continue = True
     while game_continue:
@@ -19,7 +28,9 @@ def main():
             encoder(choose_password)
             print('Your password has been encoded and stored!')
         elif menu_option == 2:
-            pass
+            encoded_password = encoder(choose_password)
+            decoded_password = decode(encoded_password)
+            print(f'\nThe encoded password is {encoded_password}, and the original password is {decoded_password}')
         else:
             break
 if __name__ == '__main__':
